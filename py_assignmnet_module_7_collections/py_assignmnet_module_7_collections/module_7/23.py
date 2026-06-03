c269dd9a1284027a8b9e6d5421eaf0824321a58b
tulsiirajput1113 <tulsirajput83204@gmail.com>
2026-06-03 11:50:03 +0530

    Update dictionary values and keys in 23.py

--- py_assignmnet_module_7_collections/py_assignmnet_module_7_collections/module_7/23.py
+++ py_assignmnet_module_7_collections/py_assignmnet_module_7_collections/module_7/23.py
@@ -3,11 +3,11 @@
 # Write a Python program to create a dictionary with 6 key-value pairs.
 
 my_dict = {
-    "name": "Jinal",
-    "age": 21,
+    "name": "tulsi",
+    "age": 20,
     "city": "Ahmedabad",
     "course": "Python",
-    "marks": 85,
+    "marks": 80,
     "grade": "A"
 }
 
